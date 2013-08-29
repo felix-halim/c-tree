303fcd2ecbccf0662dfb7ecc32ee932ce85fc1e7
Felix Halim <felix.halim@gmail.com>
2013-08-28 22:22:07 -0700

    where is the bug?

--- src/ctree_exp_leafsize.cc
+++ src/ctree_exp_leafsize.cc
@@ -18,11 +18,12 @@ void init(int *arr, int N) {
   for (int i = 0; i < N; i++) {
     c.insert(arr[i]);
   }
+  assert(c.check());
   // while (c.optimize());
   // c.compact();
   // while (c.optimize());
-  // c.debug();
-  // fprintf(stderr, "depth = %d, slack = %d\n", c.max_depth(), c.slack());
+  // // c.debug();
+  fprintf(stderr, "depth = %d, slack = %d\n", c.max_depth(), c.slack());
   locked = 1;
 }
 
@@ -45,4 +46,6 @@ int query(int value) {
 void results(double insert_time, double query_time, int checksum) {
   printf("%.6lf,%.6lf,%d,", insert_time, query_time, checksum);
   printf("\"%s\",%d,%d,%d,%.6lf,%.6lf,%.6lf\n", c.version, nLeaves, nCap, nInternals, c.t1, c.t2, c.t3);
+  fprintf(stderr, "depth = %d, slack = %d\n", c.max_depth(), c.slack());
+  assert(c.check());
 }
