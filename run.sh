10af6307d28ece2771a4981e2113d14810aa6046
Felix Halim <felix.halim@gmail.com>
2013-09-02 00:43:54 -0700

    .

--- run.sh
+++ run.sh
@@ -17,16 +17,13 @@ batch_noup)		./run.sh noup ctree_uniform | tee -a $out
 							./run.sh noup ctree | tee -a $out
 							./run.sh noup ctree_exp_leafsize | tee -a $out
 							./run.sh noup ctree_eager | tee -a $out
-							./run.sh noup comb2 | tee -a $out
 							./run.sh noup btree_google | tee -a $out
 							./run.sh noup sort | tee -a $out
 							./run.sh noup btree_stx | tee -a $out
 							;;
 
-batch_lfhv)		./run.sh lfhv comb | tee -a $out
-							./run.sh lfhv ctree | tee -a $out
-							./run.sh lfhv ctree_uniform | tee -a $out
-							./run.sh lfhv comb2 | tee -a $out
+batch_lfhv)		#./run.sh lfhv comb | tee -a $out
+							./run.sh lfhv ctree_64_128 | tee -a $out
 							./run.sh lfhv btree_google | tee -a $out
 							./run.sh lfhv btree_stx | tee -a $out
 							;;
