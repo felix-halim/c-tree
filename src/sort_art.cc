f0807b5493d0788be879f063cd50cf685da992a3
Felix Halim <felix.halim@gmail.com>
2013-11-09 21:32:55 -0800

    .

---
+++ src/sort_art.cc
@@ -0,0 +1,62 @@
+#include <cstdio>
+#include <cassert>
+#include <algorithm>
+#include "art.h"
+#include "test.h"
+
+inline uintptr_t getLeafValue(Node* node) {
+   // The the value stored in the pseudo-leaf
+   return reinterpret_cast<uintptr_t>(node)>>1;
+}
+
+using namespace std;
+
+Node* tree = NULL;
+
+void init(int *arr, int N) {
+  int *t = new int[N];
+  memcpy(t, arr, sizeof(int) * N);
+  sort(t, t + N);
+  for (int i = 0; i < N; i++) {
+    insert(t[i]);
+  }
+  delete[] t;
+  // art_debug = 1;
+}
+
+void insert(int value) {
+  uint8_t key[8];
+  uint64_t value64 = value;
+  loadKey(value64, key);
+  insert(tree,&tree,key,0,value64,8);
+}
+
+void erase(int value) {
+  uint8_t key[8];
+  uint64_t value64 = value;
+  loadKey(value64, key);
+  // assert(lookup(tree,key,8,0,8));
+  erase(tree,&tree,key,8,0,8);
+  // assert(!lookup(tree,key,8,0,8));
+}
+
+int lower_bound(int value) {
+  uint64_t value64 = value;
+  uint8_t key[8];
+  loadKey(value64, key);
+   
+  Node* leaf=lower_bound(tree,key,8,0,8);
+  int ret = 0;
+  if (isLeaf(leaf)) {
+    ret = getLeafValue(leaf);
+    // fprintf(stdout, "%lld (%lld)\n", ret, value);
+  }
+  return ret;
+}
+
+int select(int a, int b) {
+  return lower_bound(a) + lower_bound(b);
+}
+
+void results(Statistics &s) {
+}
