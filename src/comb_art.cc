72b4a20b8d02445fb7de700bf19f147fc3f201c6
Felix Halim <felix.halim@gmail.com>
2013-11-24 10:52:08 -0800

    .

--- src/comb_art.cc
+++ src/comb_art.cc
@@ -4,8 +4,8 @@
 #define LARGE_SIZE 4096
 #define SMALL_SIZE 64
 
-#define LARGE_TOUCH 400
-#define SMALL_TOUCH 100
+#define LARGE_TOUCH 0
+#define SMALL_TOUCH 1000
 
 #include "comb_art.h"
 #include "test.h"
