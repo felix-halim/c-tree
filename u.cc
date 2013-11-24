442b9834276ee9fedd4f8a5d6a887a0c531e0628
Felix Halim <felix.halim@gmail.com>
2013-11-24 15:10:12 -0800

    .

---
+++ u.cc
@@ -0,0 +1,35 @@
+#include <cstdio>
+
+#include <set>
+
+using namespace std;
+
+#define BATCH 1000000
+
+int main() {
+  FILE *in = fopen("rb", "data/skyserver.data");
+  FILE *out = fopen("wb", "data/skyserver.udata");
+  int tmp[BATCH] = { 0 };
+  int wtmp[BATCH];
+  set<int> s;
+  int ndup = 0;
+  while (!feof(in)) {
+    int N = fread(tmp, sizeof(int), BATCH, in);
+    int nt = 0;
+    for (int i = 0; i < N; i++) {
+      if (s.count(tmp[i])) {
+        ndup++;
+      } else {
+        s.insert(tmp[i]);
+        wtmp[nt++] = tmp[i];
+      }
+    }
+    if (nt > 0) {
+      fwrite(wtmp, sizeof(int), nt, out);
+    }
+    fprintf(stderr, "ndup = %d\n", ndup);
+  }
+  fclose(out);
+  if (ferror(in)) { fprintf(stderr,"Error reading file!\n"); }
+  if (feof(in)) { fclose(in); in = NULL; }
+}
