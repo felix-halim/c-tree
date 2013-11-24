99c97d7009428489549623ea3bb1849a1e33e30b
Felix Halim <felix.halim@gmail.com>
2013-11-24 09:45:44 -0800

    .

--- src/util.h
+++ src/util.h
@@ -22,10 +22,10 @@ static vector<long long> generate_samples(long long MAXQ) {
   vector<long long> samples;
   samples.push_back(0);
   samples.push_back(MAXQ);
-  for (long long i = 1; i <= MAXQ; i *= 2) samples.push_back(i);
+  // for (long long i = 1; i <= MAXQ; i *= 2) samples.push_back(i);
   for (long long i = 1; i <= MAXQ; i *= 10) {
     samples.push_back(i);
-    for (long long j = 0; j < 100; j += 5) samples.push_back(i * j / 100);
+    // for (long long j = 0; j < 100; j += 5) samples.push_back(i * j / 100);
   }
   sort(samples.begin(), samples.end());
   samples.erase(unique(samples.begin(), samples.end()), samples.end());
