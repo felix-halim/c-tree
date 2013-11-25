614213b8d7151e971ea40583328c8d9e7a32a220
Felix Halim <felix.halim@gmail.com>
2013-11-24 18:45:57 -0800

    .

--- unik.cc
+++ unik.cc
@@ -11,16 +11,17 @@ using namespace std;
 #define BATCH 10000000
 
 int tmp1[BATCH];
+set<int> s;
 
 int main() {
-  FILE *in1 = fopen("data/skyserver.data", "rb");
+  FILE *in1 = fopen("data/skyserver.u5data", "rb");
   assert(in1);
-  int mx = 0;
   for (int nth = 0; !feof(in1); nth++) {
+    fprintf(stderr, "nth = %d\n", nth);
     int N1 = fread(tmp1, sizeof(int), BATCH, in1);
     for (int i = 0; i < N1; i++) {
-      mx = max(mx, tmp1[i]);
+      assert(!s.count(tmp1[i]));
+      s.insert(tmp1[i]);
     }
-    fprintf(stderr, "nth = %d, %d\n", nth, mx);
   }
 }
