acc9835660fb6ca92fff09f6ca303fe322957785
Felix Halim <felix.halim@gmail.com>
2017-07-04 23:45:59 -0700

    adopt blockquicksort

--- src/partition.cc
+++ src/partition.cc
@@ -1,7 +1,7 @@
 /*
 To run, go to the root folder and execute:
 
-make ctree_sort
+make partition
 */
 
 #include <cassert>
@@ -12,223 +12,229 @@ make ctree_sort
 #include <vector>
 
 #include "random.h"
-#include "ska_sort.h"
 #include "time_it.h"
 
-using namespace std;
-
-static Random rng(140384);
-
-static void ctree_sort(long long *arr, long long *tmp, int n, int depth) {
-  if (n < 30) {
-    sort(arr, arr + n);
-    return;
-  }
-
-  int nlo = 0;
-
-  {
-    long long P = arr[rng.nextInt(n)];
-    int lo[n], hi[n];
-    int nhi = n - 1;
-    for (int i = 0; i < n; i++) {
-      lo[nlo] = hi[nhi] = i;
-      tmp[nhi] = arr[i];
-      int j = arr[i] < P;
-      nhi -= 1 - j;
-      nlo += j;
-    }
-    for (int i = n - 1, j = nlo - 1; hi[i] < nlo; i--, j--) {
-      arr[hi[i]] = arr[lo[j]];
-    }
-  }
-
-  ctree_sort(arr, tmp, nlo, depth + 1);
-  ctree_sort(tmp + nlo, arr + nlo, (n - nlo), depth + 1);
-  memcpy(arr + nlo, tmp + nlo, sizeof(long long) * (n - nlo));
-}
-
-// Best so far.
-static void ctree_sort2(long long *arr, long long *tmp, int n, int depth) {
-  if (n < 30) {
-    sort(arr, arr + n);
-    return;
-  }
-
-  long long P = arr[rng.nextInt(n)];
-  int nlo = 0, nhi = n - 1;
-  for (int i = 0; i < n; i++) {
-    tmp[nlo] = tmp[nhi] = arr[i];
-    int j = arr[i] < P;
-    nhi -= 1 - j;
-    nlo += j;
-  }
-
-  ctree_sort2(tmp, arr, nlo, depth + 1);
-  ctree_sort2(tmp + nlo, arr + nlo, (n - nlo), depth + 1);
-
-  memcpy(arr, tmp, sizeof(long long) * nlo);
-  memcpy(arr + nlo, tmp + nlo, sizeof(long long) * (n - nlo));
-}
-
-static void ctree_sort3(long long *arr, int n, int depth) {
-  if (n < 30) {
-    sort(arr, arr + n);
-    return;
-  }
-
-  long long p1 = arr[rng.nextInt(n)];
-  long long p2 = arr[rng.nextInt(n)];
-  if (p1 > p2) swap(p1, p2);
-  int cnt[3] { 0 };
-  for (int i = 0; i < n; i++) {
-    cnt[(arr[i] >= p1) + (arr[i] >= p2)]++;
-  }
-  assert(cnt[0] + cnt[1] + cnt[2] == n);
-
-  int idx[3] { 0, cnt[0], cnt[0] + cnt[1] };
-  for (int k = 0, cum = cnt[k]; k < 3; k++, cum += cnt[k]) {
-    while (idx[k] < cum) {
-      for (int i = idx[k]; i < cum; i++) {
-        int j = (arr[i] >= p1) + (arr[i] >= p2);
-        swap(arr[i], arr[idx[j]++]);
-      }
-    }
-    assert(idx[k] == cum);
-  }
-
-  ctree_sort3(arr, cnt[0], depth + 1);
-  ctree_sort3(arr + cnt[0], cnt[1], depth + 1);
-  ctree_sort3(arr + cnt[0] + cnt[1], cnt[2], depth + 1);
-}
-
-static void ctree_sort4(long long *arr, int n, int depth) {
-  if (n < 30) {
-    sort(arr, arr + n);
-    return;
-  }
-
-  long long P = arr[rng.nextInt(n)];
-  long long tmp[n];
-  int nlo = 0, nhi = n - 1;
-  for (int i = 0; i < n; i++) {
-    tmp[nlo] = tmp[nhi] = arr[i];
-    int j = arr[i] < P;
-    nhi -= 1 - j;
-    nlo += j;
-  }
-
-  ctree_sort4(tmp, nlo, depth + 1);
-  ctree_sort4(tmp + nlo, (n - nlo), depth + 1);
-
-  memcpy(arr, tmp, sizeof(long long) * nlo);
-  memcpy(arr + nlo, tmp + nlo, sizeof(long long) * (n - nlo));
-}
-
-inline unsigned long long to_unsigned(long long l) {
-  return static_cast<unsigned long long>(l) +
-         static_cast<unsigned long long>(1ll << (sizeof(long long) * 8 - 1));
-}
-
-inline long long to_signed(unsigned long long l) {
-  return l -
-         static_cast<unsigned long long>(1ll << (sizeof(long long) * 8 - 1));
-}
-
-static void radix_sort(long long *arr, int n, int depth) {
-  if (n < 256) {
-    sort(arr, arr + n);
-    return;
-  }
-
-  int occ[256]{0};
-
-  int nshift = (7 - depth) * 8;
-  int n16 = (n / 16) * 16;
-  for (int i = 0; i < n16; i+=16) {
-    for (int j = 0; j < 16; j++) {
-      occ[(to_unsigned(arr[i+j]) >> nshift) & 255]++;
-    }
-  }
-  for (int i = n16; i < n; i++) {
-    occ[(to_unsigned(arr[i]) >> nshift) & 255]++;
-  }
-
-  int idx[256]{0};
-  for (int i = 1; i < 256; i++) {
-    idx[i] = idx[i - 1] + occ[i - 1];
-  }
-  for (int i = 1; i < 256; i++) {
-    occ[i] += occ[i - 1];
-  }
-
-  for (int p = 255; p >= 0; p--) {
-    while (idx[p] < occ[p]) {
-      int rem = occ[p] - idx[p];
-      // fprintf(stderr, "p = %d, rem = %d\n", p, rem);
-      for (int i = 0, j = idx[p]; i < rem; i++, j++) {
-        int k = (to_unsigned(arr[j]) >> nshift) & 255;
-        swap(arr[idx[k]++], arr[j]);
-      }
-    }
-  }
-
-  for (int i = 0; i < 256; i++) {
-    // fprintf(stderr, "%d => %d %d\n", i, idx[i], occ[i]);
-    assert(idx[i] == occ[i]);
-    int prev = i == 0 ? 0 : occ[i - 1];
-    int sz = occ[i] - prev;
-    if (sz > 0) { 
-      radix_sort(arr + prev, sz, depth + 1);
-    }
-  }
-  // fprintf(stderr, "%d %d %d\n", cnt, n, depth);
-}
-
-// #define BSIZE 8192
-#define BSIZE 100000000
+#define BSIZE (1 << 12)
+#define block_size 64
 
 static long long arr[BSIZE];
-static long long tmp[BSIZE];
+
+int std_partition(long long p) {
+  return int(
+      std::partition(arr, arr + BSIZE, [p](long long v) { return v < p; }) -
+      arr);
+}
+
+template <typename RandomAccessIterator>
+void swap_offsets(RandomAccessIterator first, RandomAccessIterator last,
+                  unsigned char* hi, unsigned char* lo, int num,
+                  bool use_swaps) {
+  typedef typename std::iterator_traits<RandomAccessIterator>::value_type T;
+  if (use_swaps) {
+    // This case is needed for the descending distribution, where we need
+    // to have proper swapping for pdqsort to remain O(n).
+    for (int i = 0; i < num; ++i) {
+      std::iter_swap(first + hi[i], last - lo[i]);
+    }
+  } else if (num > 0) {
+    RandomAccessIterator l = first + hi[0];
+    RandomAccessIterator r = last - lo[0];
+    T tmp(*l);
+    *l = *r;
+    for (int i = 1; i < num; ++i) {
+      l = first + hi[i];
+      *r = (*l);
+      r = last - lo[i];
+      *l = (*r);
+    }
+    *r = (tmp);
+  }
+}
+
+long long* partition_right_branchless(long long* begin, long long* end,
+                                      long long pivot) {
+  long long* first = begin;
+  long long* last = end;
+  auto comp = std::less<long long>();
+  while (first < last && comp(*first, pivot)) first++;
+  while (first < last && !comp(*(last - 1), pivot)) last--;
+
+  // The branchless partitioning is derived from "BlockQuicksort: How Branch
+  // Mispredictions don’t affect Quicksort" by Stefan Edelkamp and Armin Weiss.
+  unsigned char hi[block_size];
+  unsigned char lo[block_size];
+  int nhi = 0, nlo = 0, ihi = 0, ilo = 0;
+
+  while (last - first > 2 * block_size) {
+    if (nhi == 0) {
+      ihi = 0;
+      long long* it = first;
+      for (unsigned char i = 0; i < block_size;) {
+        hi[nhi] = i++;
+        nhi += !comp(*it++, pivot);
+        hi[nhi] = i++;
+        nhi += !comp(*it++, pivot);
+        hi[nhi] = i++;
+        nhi += !comp(*it++, pivot);
+        hi[nhi] = i++;
+        nhi += !comp(*it++, pivot);
+        hi[nhi] = i++;
+        nhi += !comp(*it++, pivot);
+        hi[nhi] = i++;
+        nhi += !comp(*it++, pivot);
+        hi[nhi] = i++;
+        nhi += !comp(*it++, pivot);
+        hi[nhi] = i++;
+        nhi += !comp(*it++, pivot);
+      }
+    }
+    if (nlo == 0) {
+      ilo = 0;
+      long long* it = last;
+      for (unsigned char i = 0; i < block_size;) {
+        lo[nlo] = ++i;
+        nlo += comp(*--it, pivot);
+        lo[nlo] = ++i;
+        nlo += comp(*--it, pivot);
+        lo[nlo] = ++i;
+        nlo += comp(*--it, pivot);
+        lo[nlo] = ++i;
+        nlo += comp(*--it, pivot);
+        lo[nlo] = ++i;
+        nlo += comp(*--it, pivot);
+        lo[nlo] = ++i;
+        nlo += comp(*--it, pivot);
+        lo[nlo] = ++i;
+        nlo += comp(*--it, pivot);
+        lo[nlo] = ++i;
+        nlo += comp(*--it, pivot);
+      }
+    }
+
+    // Swap elements and update block sizes and first/last boundaries.
+    int num = std::min(nhi, nlo);
+    swap_offsets(first, last, hi + ihi, lo + ilo, num, nhi == nlo);
+    nhi -= num;
+    nlo -= num;
+    ihi += num;
+    ilo += num;
+    if (nhi == 0) first += block_size;
+    if (nlo == 0) last -= block_size;
+  }
+
+  int l_size = 0, r_size = 0;
+  int unknown_left = int((last - first) - ((nlo || nhi) ? block_size : 0));
+  if (nlo) {
+    // Handle leftover block by assigning the unknown elements to the other
+    // block.
+    l_size = unknown_left;
+    r_size = block_size;
+  } else if (nhi) {
+    l_size = block_size;
+    r_size = unknown_left;
+  } else {
+    // No leftover block, split the unknown elements in two blocks.
+    l_size = unknown_left / 2;
+    r_size = unknown_left - l_size;
+  }
+
+  // Fill offset buffers if needed.
+  if (unknown_left && !nhi) {
+    ihi = 0;
+    long long* it = first;
+    for (unsigned char i = 0; i < l_size;) {
+      hi[nhi] = i++;
+      nhi += !comp(*it++, pivot);
+    }
+  }
+  if (unknown_left && !nlo) {
+    ilo = 0;
+    long long* it = last;
+    for (unsigned char i = 0; i < r_size;) {
+      lo[nlo] = ++i;
+      nlo += comp(*--it, pivot);
+    }
+  }
+
+  int num = std::min(nhi, nlo);
+  swap_offsets(first, last, hi + ihi, lo + ilo, num, nhi == nlo);
+  nhi -= num;
+  nlo -= num;
+  ihi += num;
+  ilo += num;
+  if (nhi == 0) first += l_size;
+  if (nlo == 0) last -= r_size;
+
+  // We have now fully identified [first, last)'s proper position. Swap the last
+  // elements.
+  if (nhi) {
+    while (nhi--) std::iter_swap(first + hi[ihi + nhi], --last);
+    first = last;
+  }
+  if (nlo) {
+    while (nlo--) std::iter_swap(last - lo[ilo + nlo], first), ++first;
+    last = first;
+  }
+
+  // Put the pivot in the right place.
+  return first;
+}
+
+int fusion(long long p) {
+  return int(partition_right_branchless(arr, arr + BSIZE, p) - arr);
+}
+
+int lo[BSIZE], hi[BSIZE];
+int marker(long long p) {
+  int nhi = 0, nlo = 0;
+  for (int i = 0; i < BSIZE; i++) {
+    hi[nhi] = i;
+    nhi += (arr[i] >= p);
+  }
+
+  for (int i = 0; i < BSIZE; i++) {
+    lo[nlo] = i;
+    nlo += (arr[i] < p);
+  }
+
+  int* hip = hi;
+  int* lop = lo + nlo - 1;
+ 
+  int m = std::min(nhi, nlo);
+  while (m-- && *hip < *lop) {
+    std::swap(arr[*(hip++)], arr[*(lop--)]);
+  }
+
+  return nlo;
+}
 
 int main() {
   Random rng(140384);
 
   double total_time = 0;
   long long chk = 0;
-  for (int m = 0; m < 1; m++) {
+  for (int m = 0; m < 200000; m++) {
     for (int i = 0; i < BSIZE; i++) {
       arr[i] = rng.nextLong();
     }
 
+    long long p = arr[rng.nextInt(BSIZE)];
+    int pos = -1;
     total_time += time_it([&]() {
-      // ctree_sort(arr, tmp, BSIZE, 0);
-      // sort(arr, arr + BSIZE);
-
-      // sort(arr, arr + BSIZE, [](long long a, long long b) {
-      //   return to_unsigned(a) < to_unsigned(b);
-      // });
-
-      ctree_sort2(arr, tmp, BSIZE, 0);
-      // ctree_sort3(arr, BSIZE, 0);
-
-      // ska_sort(arr, arr + BSIZE, [&](const long long &e) { return e; });
-
-      // radix_sort(arr, BSIZE, 0);
-
-      // ctree_sort4(arr, BSIZE, 0);
+      // pos = std_partition(p);
+      pos = fusion(p);
+      // pos = marker(p);
     });
-    fprintf(stderr, "done\n");
 
-    chk = chk * 13 + arr[0];
-    for (int i = 1; i < BSIZE; i++) {
-      // assert(arr[i - 1] < arr[i]);
-      chk = chk * 13 + arr[i];
+    // fprintf(stderr, "pos = %d\n", pos);
+    for (int i = 0; i < BSIZE; i++) {
+      // fprintf(stderr, "%4d %4d %20lld, %20lld\n", i, pos, arr[i], p);
+      assert((i < pos) ? (arr[i] < p) : (arr[i] >= p));
     }
-    fprintf(stderr, "done2\n");
+
+    chk = chk * 13 + pos;
   }
 
   fprintf(stderr, "tt = %.6lf, chk = %lld\n", total_time, chk);
-  assert(chk == 894150326305700963LL);
-  // assert(chk == -3719803178456933530LL);
+  assert(chk == 2144819404491265167LL);
 }
